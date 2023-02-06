a2dbf3cb2db773eaa0e3f3a5513edf4ccf325bd0
Alan Sampson <>
2023-02-06 19:01:48 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <queue>
+#include <deque>
+#include <list>
+#include <algorithm>
+
+using namespace std::literals::string_literals;
+
+static
+constexpr
+auto clc_show = [](auto & clc, auto const & tt) {
+  std::cout << tt;
+  std::for_each(clc.cbegin(), clc.cend(), [](auto const & v_) {
+    std::cout << std::setw(3) << v_;
+  });
+  std::cout << std::endl;
+};
+
+auto q_show = [](auto & q_, auto const & tt) {
+  std::cout << tt;
+  for (auto e_ { 0ul }; e_ < q_.size(); ++e_) {
+    auto emt = std::move(q_.front());
+    q_.pop();
+    std::cout << std::setw(3) << emt;
+    q_.push(std::move(emt));
+  }
+  std::cout << std::endl;
+};
+
+int main(int argc, char const * argv[]) {
+  std::cout << "DequeBackedQueue!\n"s;
+
+  auto dq = std::deque<int> { 2, 4, 5, 7, 3, 9, };
+  auto qq = std::queue<int, std::deque<int>>(dq);
+
+  std::cout << "presort\n";
+  clc_show(dq, "dq: "s);
+  q_show(qq, "qq: "s);
+  std::cout.put('\n');
+
+  std::cout << "sorted\n"s;
+  std::sort(dq.begin(), dq.end());
+  clc_show(dq, "dq: "s);
+  q_show(qq, "qq: "s);
+  std::cout.put('\n');
+
+  while (qq.size() > 0) {
+    std::cout << "dq sz = "s << dq.size() << ", "s
+              << "qq sz = "s << qq.size() << '\n';
+    std::cout << "qq front: "s
+              << qq.front()
+              << '\n';
+    qq.pop();
+    std::cout << "dq sz = "s << dq.size() << ", "s
+              << "qq sz = "s << qq.size() << '\n';
+    clc_show(dq, "dq: "s);
+    q_show(qq, "qq: "s);
+    std::cout.put('\n');
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
